d1874499aedcc376a18d82f8eff3b4a4d5aff000
Alok Singh <aloksingh@Aloks-MacBook-Pro.local>
2016-01-10 14:56:13 -0800

    hess phi and bound calc added

--- src/optimizeLBFGS/hessPhiBound.scala
+++ src/optimizeLBFGS/hessPhiBound.scala
@@ -2,30 +2,60 @@ package optimizeLBFGS
 
 object hessPhiBound {
   import breeze.linalg._
-  import breeze.numerics.{exp, log, sqrt}
+  import breeze.numerics.{exp, log, sqrt, abs}
   import breeze.optimize._
   
-  // col vec eta, mat beta, col v doc_ct, col v mu, mat siginv, col v sigmaentropy
-  def lhoodFunction(eta: DenseVector[Double], beta: DenseMatrix[Double], doc_ct: DenseVector[Double], 
+  // colvec eta, mat beta, colvec v doc_ct, colvec mu, mat siginv, colvec sigmaentropy
+  def hessPhiBoundFunction(eta: DenseVector[Double], beta: DenseMatrix[Double], doc_ct: DenseVector[Double], 
       mu: DenseVector[Double], siginv: DenseMatrix[Double], sigmaentropy: DenseVector[Double]): 
       Tuple3[DenseMatrix[Double],Tuple2[DenseVector[Double], DenseMatrix[Double]],Double] = {
    
       val expetaCol = new DenseVector(eta.data ++ Array(0.0)).map(x => exp(x))
       val theta     = expetaCol/sum(expetaCol)
       
-      val EB1  = beta.copy
-      val EB2 = EB1(::, *) :* expetaCol
-      val EB = EB2(*, ::) :* (sqrt(doc_ct) / sum(EB2(::, *)).toDenseVector)
+      val EBa  = beta.copy
+      val EBb  = EBa(::, *) :* expetaCol
+      val EB   = EBb(*, ::) :* (sqrt(doc_ct) / sum(EBb(::, *)).toDenseVector)
       
       val hess = EB*EB.t - (theta*theta.t)*sum(doc_ct)
 
       diag(hess) -= sum(EB(*,::)) - theta * sum(doc_ct)
    
-      val hessian = hess(0 to hess.rows - 2, 0 to hess.cols - 2) + siginv
+      val hessianM = hess(0 to hess.rows - 2, 0 to hess.cols - 2) + siginv
       
-      val nu = DenseMatrix.rand[Double](hessian.rows, hessian.cols)
+      var nu = DenseMatrix.rand[Double](hessianM.rows, hessianM.cols)
       
+      //perform Cholesky
+      try  { 
+             nu = cholesky(hessianM).t    //upper triangular
+           } catch {
       
-    (EB, (eta, nu), bound)
+             case e: Exception => { 
+                         //update diagonal and recalculate cholesky
+                         val dvec = diag(hessianM)
+                         
+                         val pos = abs(hessianM)
+                         val magnitudes = sum(pos(*,::)) - abs(dvec)
+                         
+                         val dvecMin = DenseVector.rand[Double](dvec.length)
+                         
+                         for( i <- 0 until dvec.length ) { 
+                               dvecMin(i) = min(dvec(i), magnitudes(i))
+                         }
+                         
+                         diag(hessianM) := dvecMin 
+                         nu = cholesky(hessianM).t
+                          
+                         }//end exception
+             }//end catch
+           
+             val detTerm : Double = sum(log(diag(nu))) * -1.0
+             val nuInverted       = inv(upperTriangular(nu))   //check do we need upper triangular
+             val nuFinal          = nuInverted * nuInverted.t
+             val diff             = eta - mu
+             val bound            = log(theta.toDenseMatrix * beta)*doc_ct + detTerm - diff.t * siginv * diff * 0.5 - sigmaentropy
+             //toDenseMatrix performs transpose so no explicit transpose needed
+              
+    (EB, (eta, nu), bound(0))
   }
 }
