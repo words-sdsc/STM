06af6ef11915be7712790cf49891194e1ca477cf
Alok Singh <a1singh@ucsd.edu>
2016-04-11 13:54:28 -0700

    partitioning of doc set implemented

--- src/optimizeLBFGS/STMModel.scala
+++ src/optimizeLBFGS/STMModel.scala
@@ -3,45 +3,81 @@ package optimizeLBFGS
 import org.apache.spark.rdd.RDD
 import org.apache.spark.mllib.linalg.{Vector}
 import breeze.linalg.{DenseMatrix, DenseVector}
+import breeze.optimize.LBFGS
+import org.apache.spark.{SparkContext, SparkConf}
+import breeze.linalg.{Matrix, diag, inv, sum, det, cholesky}
+import breeze.numerics.{log}
 
+// Update: - the 'estep' is implemented in this class
 class STMModel {
   //GG = set of global parameters
   
-  
-  //document storage
-  private var docs: RDD[(Long, Vector)]= null
-  
-  /* ********************* run STM over a set of documents ********************* */
-  def runSTM(setOfDocs: RDD[(Long, Vector)]): STMModel = {
     
+  /* ********************* run STM over a set of documents ********************* */
+  def runSTM(documents: List[DenseMatrix[Double]], betaIndex: DenseVector[Int], 
+      updateMu: Boolean, beta: List[DenseMatrix[Double]], lambdaOld: DenseMatrix[Double],
+      mu: DenseMatrix[Double], sigma: DenseMatrix[Double],
+      verbose: Boolean) : STMModel = { 
+    
+    //Spark
+    val conf   = new SparkConf().setAppName("Spark Pi").setMaster("local")
+    val spark  = new SparkContext(conf)
+    val documentsRDD: RDD[(DenseMatrix[Double], Int)] = spark.parallelize(documents.zipWithIndex)
+      
     //get copy of global parameters
     
-    //broadcast large data structures to each node if they are needed by every doc
+
     
-    //partition set of documents
-    val metricsFromPartitions: RDD[(DenseMatrix[Double], List[DenseVector[Double]])] =   
-      setOfDocs.mapPartitions 
+    //declare the constants
+    val V = beta(1).cols
+    val K = beta(1).rows
+    val N = documents.length
+    val A = beta.length //several docs could have same beta, hence A and N could be different
+    val (siginv, sigmaentropy) = getSigma(sigma: DenseMatrix[Double])
+    
+    //broadcast any large data structures to each node if they are needed by every doc
+    
+    
+    //partition the set of documents
+    val metricsFromPartitions: RDD[(DenseMatrix[Double], DenseVector[DenseMatrix[Double]], 
+                                    DenseVector[Double], DenseMatrix[Double])] =   
+      documentsRDD.mapPartitions 
       {
-        docs =>
-                val size             = 5                       //random for now
-                // TP = set of accumulators for this partition
-                val partition_accum1 = DenseMatrix.zeros[Double](size,size)
-                var partition_accum2 = List[DenseVector[Double]]()
+        docs => 
                 
-                docs.foreach 
-                {
-                    case(id: Long, termCounts: Vector) =>
-                      /* send this document for STM processing,
-                         DD = set of results from STM for this single document */
-                      val i = 1
+                //TP = set of variables specific to this partition
+                val sigma_pt   =  DenseMatrix.zeros[Double](K,K) 
+                val beta_pt    =  DenseVector.zeros[DenseMatrix[Double]](A)  //List of Matrices
+                for(i <- 0 until beta_pt.size) { beta_pt(i) = DenseMatrix.zeros[Double](K,V) } 
+                val bound_pt   = DenseVector.zeros[Double](N)
+                val lambda_pt  = DenseMatrix.zeros[Double](K-1, N)  
+                //every COL is a lambda of len K-1, transpose it before returning 
+                
+                docs.foreach { 
+                  case (doc: DenseMatrix[Double], indx: Int) =>  
+                       
+                    val wordIndices   = (doc(0, ::).t).data.map(_.toInt).toIndexedSeq //1st row of doc = indices of words
+                    val aspect = betaIndex(indx)
+                    val init   = lambdaOld(indx,::).t //matrix N x K-1, each row is a lambda for a doc
+                    //if(updateMu)  //check if we need this
+                    val mu_j   = mu(::,indx) 
+                    val beta_j = beta(aspect)(::, wordIndices).toDenseMatrix
                     
+                    /* infer this document via STM,
+                       DD = set of results from STM for this single document */
+                    val results = logisticnormal(init, mu_j, siginv, beta_j, doc(1,::).t, sigmaentropy)
+                                                                              //doc(2nd row)=count of words
                     
                     // update partition accumulators TP with DD
-                    
+                    beta_pt(aspect)(::, wordIndices) += results._1.asInstanceOf[Matrix[Double]] //two docs can have same aspect
+                    lambda_pt(::, indx)     := results._2._1 //each COL is a lambda   
+                    sigma_pt                += results._2._2
+                    bound_pt(indx)           = results._3
+
                 }
 
                 // return iterator (each tuple is a set of accumulators from one partition)
-                Iterator((partition_accum1, partition_accum2))
+                Iterator((sigma_pt, beta_pt, bound_pt, lambda_pt.t))
         
       }//end mapPartitions
     
@@ -49,7 +85,6 @@ class STMModel {
       PP = aggregation of results from each partition in 'metricsFromPartitions'
       */
       
-    
     
     //update global parameters GG using these aggregates PP
     
@@ -61,10 +96,41 @@ class STMModel {
     
   } //end runSTM
   
-  
-  /* *********************initialize global parameters ********************* */
-  def initialize() = {
+   
+  /* *********************returns siginverse, sigmaentropy ********************* */
+  def getSigma(sigma: DenseMatrix[Double]) : Tuple2[DenseMatrix[Double], Double] = {
+      //pre-processing of common components
+      var sigobj       = DenseMatrix.zeros[Double](1,1)
+      var sigmaentropy = 0.0
+      val siginv       = inv(sigma)
+
+      try  { 
+             sigobj       = cholesky(sigma).t        //upper triangular
+             sigmaentropy = sum(log(diag(sigobj)))
+           } catch {
+             case e: Exception => { 
+             sigmaentropy = 0.5 * (log(det(sigma).abs)) 
+           } 
+             
+           }//end catch
     
-  }//end initialize
+      (siginv, sigmaentropy)
+  }//end getSigma
   
+  
+  /* *********************infer single doc ********************* */
+  def logisticnormal(eta: DenseVector[Double], mu: DenseVector[Double], 
+        siginv: DenseMatrix[Double], beta: DenseMatrix[Double], doc_ct: DenseVector[Double], 
+        sigmaentropy: Double): 
+      Tuple3[DenseMatrix[Double],Tuple2[DenseVector[Double], DenseMatrix[Double]],Double] = {
+    
+      val lbfgs = new LBFGS[DenseVector[Double]](tolerance = 1E-12, m=11)    //maxIter = 10000 removed tolerance = 1E-28
+      val likeliHoodF  = likelihood.lhoodFunction(beta, doc_ct, mu, siginv) 
+                         //how does it get which doc_ct is for which word in dict ?
+                         //--> adjust #cols in beta = #rows in doc_ct (drop all zero rows& maintain order)
+                         //--> beta*doc_ct
+      val newEta       = lbfgs.minimize(likeliHoodF, eta)
+      
+      hessPhiBound.evaluate(newEta, beta, doc_ct, mu, siginv, sigmaentropy )
+  }
 }
--- src/optimizeLBFGS/estep.scala
+++ src/optimizeLBFGS/estep.scala
@@ -6,7 +6,7 @@ object estep {
   import breeze.numerics.{log}
   import scala.collection.parallel.mutable.ParArray
   import org.apache.spark.{SparkContext, SparkConf}
-  //import org.apache.spark.mllib.linalg
+
 
   def evaluate(documents: List[DenseMatrix[Double]], betaIndex: DenseVector[Int], 
       updateMu: Boolean, beta: List[DenseMatrix[Double]], lambdaOld: DenseMatrix[Double],
@@ -14,7 +14,7 @@ object estep {
       verbose: Boolean): 
       Tuple4[DenseMatrix[Double], DenseVector[DenseMatrix[Double]],
         DenseVector[Double], DenseMatrix[Double] ] = {
-    
+        
     //declare the constants
     val V = beta(1).cols
     val K = beta(1).rows
@@ -22,20 +22,10 @@ object estep {
     val A = beta.length //several docs could have same beta, hence A and N could be different
     
     //initialization
-    //val sigma_g =  DenseMatrix.zeros[Double](K,K)
-    //cannot use single sigma_g, due to non-determinism of parallel summations
-    val conf = new SparkConf().setAppName("Spark Pi").setMaster("local")
-    val spark = new SparkContext(conf)
+    val sigma_g =  DenseMatrix.zeros[Double](K,K)
     
-    //global
-    val sigma_g  =  DenseVector.zeros[DenseMatrix[Double]](documents.length)
-    for(i <- 0 until sigma_g.size) { 
-      sigma_g(i) = DenseMatrix.zeros[Double](K,K) 
-    }
-    //>>
-    //val accum = spark.accumulator[DenseVector[DenseMatrix[Double]]](sigma_g, "My Accumulator")
     
-    //global
+    //global List of Matrices
     val beta_g  =  DenseVector.zeros[DenseMatrix[Double]](A)
     for(i <- 0 until beta_g.size) { 
       beta_g(i) = DenseMatrix.zeros[Double](K,V) 
@@ -63,11 +53,10 @@ object estep {
              
            }//end catch
      
-      //iterate over documents 1...N   
-      ParArray.tabulate(documents.length){ indx => g(documents(indx),indx) }
-      
-      //http://stackoverflow.com/questions/9137644/how-to-get-the-element-index-when-mapping-an-array-in-scala?lq=1
-      //http://docs.scala-lang.org/overviews/parallel-collections/overview.html
+      //iterate over documents 1...N of this partition in serial
+      documents.zipWithIndex.foreach {
+          case (doc: DenseMatrix[Double], indx: Int) => g(doc, indx) 
+      }
       
       def g(doc: DenseMatrix[Double], j: Int) = { 
           val wordIndices   = (doc(0, ::).t).data.map(_.toInt).toIndexedSeq 
@@ -82,17 +71,14 @@ object estep {
           val results = logisticnormal(init, mu_j, siginv, beta_j, doc(1,::).t, sigmaentropy)
                                                                     //doc(2nd row)=count of words
           //update the global values 
-          sigma_g(j) += results._2._2
+          sigma_g += results._2._2
           beta_g(aspect)(::, wordIndices) += results._1.asInstanceOf[Matrix[Double]] //Q? can two docs same aspect
           bound(j) = results._3
           lambda(::, j) := results._2._1 //each COL is a lambda     
       }
     
-      //this extra step is to avoid non determinism when parallel threads add to same variable
-      val sigma_g_sum = DenseMatrix.zeros[Double](K,K)
-      sigma_g.foreach { sigma_g_sum += _ } //sum of all matrices produced by all documents
       
-    (sigma_g_sum, beta_g, bound, lambda.t)
+    (sigma_g, beta_g, bound, lambda.t)
     //transpose lambda so that each ROW is a lambda of length K-1
     
   } //end of function evaluate
@@ -112,5 +98,4 @@ object estep {
       hessPhiBound.evaluate(newEta, beta, doc_ct, mu, siginv, sigmaentropy )
   }
   
-  //queue[2] -> test hessian, phi, bound calculation 
 }
