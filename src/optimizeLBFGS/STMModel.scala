05f3aee0654317a916b9717677d4750f753915e0
Alok Singh <a1singh@ucsd.edu>
2016-04-20 16:58:31 -0700

    update_mu() and update_beta() for global updates added

--- src/optimizeLBFGS/STMModel.scala
+++ src/optimizeLBFGS/STMModel.scala
@@ -2,19 +2,20 @@ package optimizeLBFGS
 
 import org.apache.spark.rdd.RDD
 import org.apache.spark.mllib.linalg.{Vector}
-import breeze.linalg.{DenseMatrix, DenseVector}
+import breeze.linalg.{DenseMatrix, DenseVector, Matrix, diag, inv, sum, det, cholesky, zipValues}
+import breeze.linalg.{*}
 import breeze.optimize.LBFGS
 import org.apache.spark.{SparkContext, SparkConf}
-import breeze.linalg.{Matrix, diag, inv, sum, det, cholesky, zipValues}
-import breeze.numerics.{log}
+import breeze.numerics.{log, abs}
  
 class STMModel {
   //GG = set of global parameters
-  
+  var beta_g : DenseMatrix[Double] = null
+  var mu_g   : Tuple2[DenseMatrix[Double], DenseMatrix[Double]] = null
     
   /* ********************* run STM over a set of documents ********************* */
   def runSTM(documents: List[DenseMatrix[Double]], betaIndex: DenseVector[Int], 
-      updateMu: Boolean, beta: List[DenseMatrix[Double]], lambdaOld: DenseMatrix[Double],
+      updateMu: Boolean, beta: DenseVector[DenseMatrix[Double]], lambdaOld: DenseMatrix[Double],
       mu: DenseMatrix[Double], sigma: DenseMatrix[Double],
       verbose: Boolean) : STMModel = { 
     
@@ -89,11 +90,14 @@ class STMModel {
       //update global parameters GG using these aggregates PP
       
         //update_mu(lambda=lambda, mode=settings$gamma$mode, covar=settings$covariates$X, settings$gamma$enet)
-          
+        val dummyCoVar : DenseMatrix[Double] = null
+        mu_g = update_mu(collect_lambda, dummyCoVar)
+        
         //update_sigma(nu=sigma.ss, lambda=lambda, mu=mu$mu, sigprior=settings$sigma$prior)
           
         //update_beta(beta.ss, beta$kappa, settings)
-    
+        beta_g = update_beta(collect_beta)
+        
     //unpersist broadcasted variables
     betaBc.unpersist()
     
@@ -101,7 +105,61 @@ class STMModel {
     
   } //end runSTM
   
-   
+    def update_sigma() = {
+       
+    }
+     
+    //***  used by update_mu() ***f() = Variational Linear Regression with a Half-Cauchy hyperprior 
+    def vbreg(Y: DenseVector[Double], X:DenseMatrix[Double]) : DenseVector[Double] = {
+      //b0=1, d0=1 taken out of function signature and replaced with constant
+      val Xcorr  = X.t * X
+      val XYcorr = X.t * Y
+      val an = (1.0 + X.rows)/2
+      val D  = X.cols
+      val N  = X.rows
+      val cn = X.cols
+      var dn : Double = 1.0
+      var Ea : Double = cn/dn
+      var ba : Double = 1.0
+      var w = DenseVector.zeros[Double](D)
+      var precision : Double = 1.0
+      var converge = 1000.0
+      
+      while(converge > 0.0001) {
+        val wOld = w.copy
+        val ppmat : DenseMatrix[Double] = diag(DenseVector.fill(D){Ea})
+        ppmat(0,0) = 0.0
+        val invV = ppmat + Xcorr :* precision
+        val V : DenseMatrix[Double] = inv(invV)
+        w = (V :* precision) * XYcorr
+        
+        val sse = sum((X * w - Y):^ 2.0)
+        val bn = (sse + sum(diag(Xcorr * V))) * 0.5 + ba
+        precision = an/bn
+        ba = 1.0/(precision + 1.0) //b0=1
+        
+        val da = 2.0 / (Ea + 1.0)  //d0=1
+        val diagV = diag(V)
+        val w1 = w(1 to -1)
+        dn = 2.0 * da + (w1.t * w1 + sum(diagV(1 to -1)))
+        Ea = cn / dn
+        converge = sum(abs(w - wOld))
+      }
+      
+      w
+    }
+      
+    def update_mu(lambda: DenseMatrix[Double], covar: DenseMatrix[Double]) : Tuple2[DenseMatrix[Double], DenseMatrix[Double]] = {
+      //gamma = apply f(on every col of lambda) and cbind the result to get gamma matrix
+      val gamma  : DenseMatrix[Double]  = lambda(::, * ).map( Y => vbreg(Y, covar) )
+      val mu     : DenseMatrix[Double]  = (covar * gamma).t
+      (mu, gamma)
+    }
+     
+    def update_beta(betaList : DenseVector[DenseMatrix[Double]]) : DenseMatrix[Double] = {
+       betaList.foldLeft(DenseMatrix.zeros[Double](betaList(0).rows, betaList(0).cols))(_ + _)
+    }
+  
   /* *********************returns siginverse, sigmaentropy ********************* */
   def getSigma(sigma: DenseMatrix[Double]) : Tuple2[DenseMatrix[Double], Double] = {
       //pre-processing of common components
